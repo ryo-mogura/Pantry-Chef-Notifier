04750c84faa79d0df6d9eccb522160066195f3e7
ryo-mogura <ryosuke48696869@gmail.com>
2024-05-16 17:49:47 +0900

    [feat] #7 i18nによる日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -10,24 +10,13 @@ Bundler.require(*Rails.groups)
 
 module Pantry_chef_notifier_app
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.helper false
       g.test_framework nil
     end
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
