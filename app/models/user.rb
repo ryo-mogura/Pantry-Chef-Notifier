33f476450b5f612a1df4c4b9e367d813d270b403
ryo-mogura <ryosuke48696869@gmail.com>
2024-05-20 15:37:47 +0900

    [fix] #8 1.ユーザ新規登録時にnameパラメーターが許可されておらず、エラーが発生していたため修正 2.ゲストユーザーにnameを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
     end
   end
 end
