e9caf3ef5ee32ec6b15192f8b3d4d8b891e61a01
ryo-mogura <ryosuke48696869@gmail.com>
2024-05-28 19:15:09 +0900

    [feat] #14 新規登録機能の実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,22 +1,36 @@
+# frozen_string_literal: true
+
 class FoodsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @foods = current_user.foods
   end
 
   def new
+    @food = current_user.foods.new
   end
 
   def create
+    @food = current_user.foods.new(food_params)
+
+    if @food.save!
+      flash[:notice] = '登録しました'
+      redirect_to foods_path
+    else
+      render :new
+    end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:id, :name, :quantity, :expiration_date, :storage)
   end
 end
