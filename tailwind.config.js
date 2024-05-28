ab9720bc6962a602042ac4cee136d0e5f72ca8e6
ryo-mogura <ryosuke48696869@gmail.com>
2024-05-28 19:18:09 +0900

    [style] #14 tailwindcssで使用できる色の追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,18 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      colors: {
+        jungle_green: 'rgb(42, 157, 144)',
+        shamrock: 'rgb(46, 194, 179)',
+        bermuda: 'rgb(127, 206, 215)',
+        aqua_island: 'rgb(159, 210, 219)',
+        alice_blue: 'rgb(240, 249, 255)',
+        linen: 'rgb(248, 233, 227)',
+        mandys_pink:'rgb(244, 210, 199)'
+      },
+    },
+  },
   plugins: [require("daisyui")],
 }
