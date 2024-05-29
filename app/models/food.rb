3307b5ff385cca6a867beb0b26148f0eee2d92f9
ryo-mogura <ryosuke48696869@gmail.com>
2024-05-29 17:10:16 +0900

    [refactor] #14 rubocopによる指摘箇所を修正

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,9 +4,8 @@ class Food < ApplicationRecord
   belongs_to :user
   enum storage: { refrigerator: 0, freezer: 1, others: 2 }
 
-  validates :name, presence:true
-  validates :expiration_date, presence:true
-  validates :storage, presence:true
-  validates :quantity, numericality: { only_integer: true }, presence:true
-
+  validates :name, presence: true
+  validates :expiration_date, presence: true
+  validates :storage, presence: true
+  validates :quantity, numericality: { only_integer: true }, presence: true
 end
