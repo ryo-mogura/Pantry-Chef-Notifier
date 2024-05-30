7b7f743f9a992121d8a340dca3d51d62acedc84e
ryo-mogura <ryosuke48696869@gmail.com>
2024-05-30 18:15:33 +0900

    [feat] #17 食材の編集・削除機能の実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -26,9 +26,27 @@ class FoodsController < ApplicationController
     @food = Food.find(params[:id])
   end
 
-  def edit; end
+  def edit
+    @food = Food.find(params[:id])
+  end
 
-  def destroy; end
+  def update
+    @food = Food.find(params[:id])
+    @food.update(food_params)
+
+    if @food.save
+      redirect_to foods_path, success: t('defaults.flash_message.edit', item: Food.model_name.human)
+    else
+      flash.now[:warning] = t('defaults.flash_message.not_edit', item: Food.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, success: t('defaults.flash_message.deleted', item: Food.model_name.human), status: :see_other
+  end
 
   private
 
