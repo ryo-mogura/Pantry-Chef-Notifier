058876316bd15dc5830664a90b1618607537bd15
ryo-mogura <ryosuke48696869@gmail.com>
2024-06-12 22:11:47 +0900

    [feat] #59 ページネーション機能の追加

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,7 +5,7 @@ class FoodsController < ApplicationController
   before_action :set_q, only: [:index]
 
   def index
-    @foods = @q.result(distinct: true)
+    @foods = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
   def new
