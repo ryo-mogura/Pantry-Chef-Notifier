e0cb35994fbae2ab013728f3f4254ac46ddb4c42
ryo-mogura <ryosuke48696869@gmail.com>
2024-06-18 09:22:18 +0900

    [feat] #73 Top画面にLineログインbtnを設置

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def devise_mapping
+    Devise.mappings[:user]
+  end
+  def resource_class
+    devise_mapping.to
+  end
+
+  def resource_name
+    devise_mapping.name
+  end
 end
