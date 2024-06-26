3f927532fe7cb4f2be0e6be58ee672c29e0c9832
ryo-mogura <ryosuke48696869@gmail.com>
2024-06-26 18:10:39 +0900

    [feat] #12 letter_opener_webの設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -35,4 +35,8 @@ Rails.application.routes.draw do
 
   # rakuten
   get 'search', to: 'rakuten_recipes#search'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
