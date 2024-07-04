934fea2b6c6f9232154fd8f97d04631684bbac83
ryo-mogura <ryosuke48696869@gmail.com>
2024-07-04 23:07:27 +0900

    [feat] #79 Line上からも画像を登録できるようにコードを追加

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -33,9 +33,9 @@ class LineBotController < ApplicationController
   end
   # ユーザー検索
   def find_user(event)
-    line_id = event['source']['userId']  # eventはメソッドのスコープ外で定義されていると仮定する
+    line_id = event['source']['userId']
     user = User.find_by(uid: line_id)
-    return user  # ユーザーが見つかった場合にそのユーザーを返す
+    return user
   end
   # メッセージ処理
   def main_event(event, user)
@@ -45,99 +45,117 @@ class LineBotController < ApplicationController
       when Line::Bot::Event::MessageType::Text
         message = create_message(event.message['text'], user)
         client.reply_message(event['replyToken'], message)
+      when Line::Bot::Event::MessageType::Image
+        handle_image_message(event, user)
       end
     end
   end
   # LineBotからユーザーへのメッセージ生成
   def create_message(text, user)
-    # user.update(status: 'idle')
     case text
     when '食材リスト'
-      food_list(send_foods_item(user), text)
+      { type: 'text', text: send_foods_item(user) }
     when '消費期限'
-      food_list(send_food_limits(user), text)
+      { type: 'text', text: send_food_limits(user) }
     when 'レシピ検索'
       recipe_branch(user)
     when '食材登録'
-      case user.status
-      when 'idle'
+      if user.status == 'idle'
         user.update(status: 'waiting_add_food_name')
         { type: 'text', text: '登録する食材名を入力してください' }
       end
     else
-      if user.status == 'waiting_for_recipe'
-        recipe_branch(user, text)
-      elsif  user.status == 'waiting_add_food_name'
-        temp_food = user.line_messages.create(temp_name: text)
-        user.update(status: 'waiting_add_food_quantity')
-        { type: 'text', text: '在庫数を数字で入力してください。' }
-      elsif user.status == 'waiting_add_food_quantity'
-
-        temp_food = user.line_messages.last
-        temp_food.update(temp_quantity: text)
-        user.update(status: 'waiting_add_food_expiration')
-        { type: 'text', text: '消費期限を入力してください。例）2024-07-25' }
-
-      elsif user.status == 'waiting_add_food_expiration'
-
-        user.update(status: 'waiting_add_food_storage')
-        temp_food = user.line_messages.last
-        temp_food.update(temp_expiration_date: text)
-        { type: 'text', text: '保存場所を数字で入力してください。0: 冷蔵庫 1: 冷凍庫 2: その他' }
-
-      elsif user.status == 'waiting_add_food_storage'
-
-        user.update(status: 'waiting_add_food_image')
-        temp_food = user.line_messages.last
-        temp_food.update(temp_storage: text)
-        { type: 'text', text: '画像を送信してください' }
-
-      elsif user.status == 'waiting_add_food_image'
-        temp_food = user.line_messages.last
-        user.update(status: 'idle')
-        food = Food.new(
-                name: temp_food.temp_name,
-                quantity: temp_food.temp_quantity,
-                expiration_date: temp_food.temp_expiration_date,
-                storage: temp_food.temp_storage.to_i,
-                user_id: user.id
-              )
-        if food.save
-          saved_food = Food.find_by(name: temp_food.temp_name, user_id: user.id)
-          response = "以下の食材が保存されました。\n\n食材名: #{saved_food.name}\n在庫数: #{saved_food.quantity}\n消費期限: #{saved_food.expiration_date}\n保存場所: #{saved_food.storage}"
-        else
-          response = 'エラーが発生しました。正しく入力してください。'
-        end
-        { type: 'text', text: response }
-      else
-        user.update(status: 'idle')
-        { type: 'text', text: 'エラーが発生しました。正しく入力してください。' }
-      end
+      handle_text_message(text, user)
     end
   end
+# 食材画像の処理
+  def handle_image_message(event, user)
+    if user.status == 'waiting_add_food_image'
+      response = client.get_message_content(event.message['id'])
+      file = Tempfile.new(['line_image', '.jpg'])
+      file.binmode
+      file.write(response.body)
+      file.rewind
+
+      temp_food = user.line_messages.last
+      food = Food.new(
+        name: temp_food.temp_name,
+        quantity: temp_food.temp_quantity,
+        expiration_date: temp_food.temp_expiration_date,
+        storage: temp_food.temp_storage.to_i,
+        user_id: user.id,
+        food_image: file
+      )
+
+      file.close
+      file.unlink
+
+      if food.save
+        response_text = "以下の食材が保存されました。\n\n食材名: #{food.name}\n在庫数: #{food.quantity}\n消費期限: #{food.expiration_date}\n保存場所: #{food.storage}"
+      else
+        response_text = 'エラーが発生しました。正しく入力してください。'
+      end
+      user.update(status: 'idle')
+      client.reply_message(event['replyToken'], { type: 'text', text: response_text })
+    else
+      client.reply_message(event['replyToken'], { type: 'text', text: 'エラーが発生しました。正しく入力してください。' })
+      user.update(status: 'idle')
+    end
+  end
+# 食材登録の対話機能の条件分岐（食材名、在庫数、消費期限、保存場所）
+  def handle_text_message(text, user)
+    case user.status
+    when 'waiting_for_recipe'
+      recipe_branch(user, text)
+    when 'waiting_add_food_name'
+      temp_food = user.line_messages.create(temp_name: text)
+      user.update(status: 'waiting_add_food_quantity')
+      { type: 'text', text: '在庫数を数字で入力してください。' }
+    when 'waiting_add_food_quantity'
+      temp_food = user.line_messages.last
+      temp_food.update(temp_quantity: text)
+      user.update(status: 'waiting_add_food_expiration')
+      { type: 'text', text: '消費期限を入力してください。例）2024-07-25' }
+    when 'waiting_add_food_expiration'
+      temp_food = user.line_messages.last
+      temp_food.update(temp_expiration_date: text)
+      user.update(status: 'waiting_add_food_storage')
+      { type: 'text', text: '保存場所を数字で入力してください。0: 冷蔵庫 1: 冷凍庫 2: その他' }
+    when 'waiting_add_food_storage'
+      temp_food = user.line_messages.last
+      temp_food.update(temp_storage: text)
+      user.update(status: 'waiting_add_food_image')
+      { type: 'text', text: '画像を送信してください' }
+    else
+      user.update(status: 'idle')
+      { type: 'text', text: 'エラーが発生しました。正しく入力してください。' }
+    end
+  end
+
   # 食材リストの取得とメッセージ生成
   def send_foods_item(user)
-    food_items =  Food.where(user_id: user.id)
-    if food_items != []
+    food_items = Food.where(user_id: user.id)
+    if food_items.any?
       names = food_items.map(&:name)
       quantity = food_items.map(&:quantity)
       result = names.zip(quantity).map { |name, qty| "#{name} : 在庫数#{qty}" }.join("\n")
-      response = "以下の食材があります。\n\n#{result}"
+      "以下の食材があります。\n\n#{result}"
     else
-      response = '食材が登録されていません。'
+      '食材が登録されていません。'
     end
   end
+
   # 期限が近づいている食材の取得とメッセージの生成
   def send_food_limits(user)
     limit_second_days = Date.today..Time.now.end_of_day + 2.days
-    limit_foods =  Food.where(user_id: user.id).where(expiration_date: limit_second_days)
-    if limit_foods != []
+    limit_foods = Food.where(user_id: user.id, expiration_date: limit_second_days)
+    if limit_foods.any?
       names = limit_foods.map(&:name)
       expiration_dates = limit_foods.map(&:expiration_date)
       result = names.zip(expiration_dates).map { |name, expiration_date| "#{name} : 消費期限#{expiration_date}" }.join("\n")
-      response = "以下の食材の消費期限が近づいています（二日以内）。\n早めに消費することをオススメします。\n\n#{result}"
+      "以下の食材の消費期限が近づいています（二日以内）。\n早めに消費することをオススメします。\n\n#{result}"
     else
-      response = '二日以内が期限の食材はありません。'
+      '二日以内が期限の食材はありません。'
     end
   end
   # レシピ検索の対話機能の条件分離
@@ -162,9 +180,4 @@ class LineBotController < ApplicationController
       end
     end
   end
-
-  # 食材の画登録処理
-  # def
-
-  # end
 end
