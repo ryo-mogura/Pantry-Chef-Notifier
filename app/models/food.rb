826313a0829013c87ce6b00a0c47eb87b8076aaf
ryo-mogura <ryosuke48696869@gmail.com>
2024-07-16 20:44:34 +0900

    [feat] #57 カテゴリー機能のvalidatesを追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Food < ApplicationRecord
+  before_validation :sanitize_category_id
+
   belongs_to :user
-  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   mount_uploader :food_image, FoodImageUploader
@@ -12,8 +13,17 @@ class Food < ApplicationRecord
   validates :expiration_date, presence: true
   validates :storage, presence: true
   validates :quantity, numericality: { only_integer: true }, presence: true
+  validates :category_id, inclusion: { in: Category.all.map(&:id) }
+
   # Ransackv4.0.0で追加された許可リストの作成
   def self.ransackable_attributes(_auth_object = nil)
     %w[created_at expiration_date name quantity storage updated_at]
   end
+
+  private
+  # カテゴリIDが1〜11かをチェック、範囲外ならnilに設定
+  def sanitize_category_id
+    self.category_id = nil unless Category.all.map(&:id).include?(category_id)
+  end
+
 end
