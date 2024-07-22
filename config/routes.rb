fe0b25b985debe2a4872cb8b0762d764d2ea519c
ryo-mogura <ryosuke48696869@gmail.com>
2024-07-22 22:18:40 +0900

    [feat] #19 admin用のroutesを設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,21 +10,29 @@ Rails.application.routes.draw do
     root to: 'tops#top', as: :unauthenticated_root
   end
 
-  # ユーザー認証関係
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    get '/' => 'homes#top', as: :root
+  end
+
   # ゲストユーザー機能でControllerをカスタマイズしているので変更
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
   devise_scope :user do
     # sign_inとsign_outのルーティングを変更する
     get 'login', to: 'devise/sessions#new'
     post 'login', to: 'devise/sessions#create'
     delete 'signout', to: 'devise/sessions#destroy'
-    # ゲストユーザーのログイン(6.18 使用しないと考えコメントアウト)
-    # post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+
   # LineBot
   post '/', to: 'line_bot#callback'
   namespace :users do
