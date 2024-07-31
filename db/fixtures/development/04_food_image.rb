3f36a5f4c36de74622baaa6e99900b76d5312766
ryo-mogura <ryosuke48696869@gmail.com>
2024-07-31 21:45:30 +0900

    [feat] #87 食材の画像の初期データを更新

--- db/fixtures/development/04_food_image.rb
+++ db/fixtures/development/04_food_image.rb
@@ -1,5 +1,10 @@
 
-FoodImage.seed do |s|
-  s.id = 1
-  s.image_url = Rails.root.join("db/fixtures/images/たまねぎ.jpg").open
-end
+FoodImage.create!([
+  { image_name: 'たまねぎ', image_url: File.open(Rails.root.join('db/fixtures/images/たまねぎ.jpg')) },
+  { image_name: 'にんじん', image_url: File.open(Rails.root.join('db/fixtures/images/にんじん.jpg')) },
+  { image_name: 'レタス', image_url: File.open(Rails.root.join('db/fixtures/images/レタス.jpg')) },
+  { image_name: 'アスパラガス', image_url: File.open(Rails.root.join('db/fixtures/images/アスパラガス.jpg')) },
+  { image_name: 'かぼちゃ', image_url: File.open(Rails.root.join('db/fixtures/images/かぼちゃ.jpg')) },
+  { image_name: 'だいこん', image_url: File.open(Rails.root.join('db/fixtures/images/だいこん.jpg')) },
+  { image_name: 'なす', image_url: File.open(Rails.root.join('db/fixtures/images/なす.jpg')) }
+])
