43d703172bc5ff58f90727448aec3da40b81e60e
ryo-mogura <ryosuke48696869@gmail.com>
2024-08-10 23:34:19 +0900

    [test] #122 ソート機能のテストコードを追加

--- spec/system/foods_spec.rb
+++ spec/system/foods_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 describe 'FoodのCRUD' do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { create(:user) } # ユーザーを定義
+  let!(:food) { create(:food, user: user, name: "テスト食材2",expiration_date: Date.today ) }
+  let!(:food2) { create(:food, user: user, name: "テスト食材1", expiration_date: Date.today + 1 ) }
+  let!(:food3) { create(:food, user: user, name: "テスト食材3", expiration_date: Date.today + 2) }
+  let!(:food_with_image) { create(:food, user: user, name: "画像付きのテスト食材", expiration_date: Date.today + 2, ) }
   before do
     sign_in(user)
   end
@@ -8,6 +12,10 @@ describe 'FoodのCRUD' do
   describe '一覧表示' do
     context '食材が登録されている場合' do
       it '登録した食材が表示される' do
+        expect(page).to have_content food.name
+        expect(page).to have_content food.expiration_date
+        expect(page).to have_content food.quantity
+        expect(page).to have_content I18n.t("activerecord.attributes.food.storages.#{food.storage}")
       end
       it '画像が登録されていない場合' do
 
@@ -16,18 +24,35 @@ describe 'FoodのCRUD' do
 
       end
       it '詳細ページへ遷移できる' do
+        click_on "詳細", match: :first
+        expect(current_path).to eq food_path(food.id)
       end
     end
 
     context 'ソート機能' do
-      it '期限が近い順にソートできる' do
-        select '消費期限が近い順', from: 'ソート順'
+      it '期限が近い順にソートできる', js: true do
+        select '消費期限が近い順', from: '並び替え'
+        expect(page).to have_text(food.name)
+        expect(page).to have_text(food2.name)
+        expect(page).to have_text(food3.name)
+        food_names = page.all('.card-title').map(&:text)
+        expect(food_names).to eq([food.name, food2.name, food3.name])
       end
-      it '期限が遠い順にソートできる' do
-        select '消費期限が遠い順', from: 'ソート順'
+      it '期限が遠い順にソートできる', js: true do
+        select '消費期限が遠い順', from: '並び替え'
+        expect(page).to have_text(food.name)
+        expect(page).to have_text(food2.name)
+        expect(page).to have_text(food3.name)
+        food_names = page.all('.card-title').map(&:text)
+        expect(food_names).to eq([food3.name, food2.name, food.name])
       end
-      it '名前順にソートできる' do
-        select 'あいうえお順', from: 'ソート順'
+      it '名前順にソートできる', js: true do
+        select 'あいうえお順', from: '並び替え'
+        expect(page).to have_text(food.name)
+        expect(page).to have_text(food2.name)
+        expect(page).to have_text(food3.name)
+        food_names = page.all('.card-title').map(&:text)
+        expect(food_names).to eq([food2.name, food.name, food3.name])
       end
     end
   end
@@ -36,13 +61,23 @@ describe 'FoodのCRUD' do
   describe '新規食材の登録' do
     context 'フォームの入力値が正常な場合' do
       it '登録に成功する' do
-
+        fill_in '食材名', with: 'テスト食材'
+        fill_in '在庫数', with: 1
+        fill_in '消費期限', with: Date.today
+        click_on '登録する'
+        expect(current_path).to eq(foods_path)
+        expect(page).to have_text('テスト食材')
       end
     end
 
-    context '食材名・在庫数・期限日が入力されていない場合' do
+    context '食材名・在庫数・期限日のいずれかが入力されていない場合' do
       it '登録に失敗する' do
-
+        fill_in '食材名', with: ''
+        fill_in '在庫数', with: 1
+        fill_in '消費期限', with: Date.today
+        click_on '登録する'
+        expect(current_path).to eq(new_food_path)
+        expect(page).to have_text('食材を作成できませんでした')
       end
     end
 
