33017294ceb4df6eadde5c84484dd6df78e9166e
ryo-mogura <ryosuke48696869@gmail.com>
2024-08-11 14:53:48 +0900

    [test] #122 食材登録機能のテストコードを追加

--- spec/system/foods_spec.rb
+++ spec/system/foods_spec.rb
@@ -4,7 +4,7 @@ describe 'FoodのCRUD' do
   let!(:food) { create(:food, user: user, name: "テスト食材2",expiration_date: Date.today ) }
   let!(:food2) { create(:food, user: user, name: "テスト食材1", expiration_date: Date.today + 1 ) }
   let!(:food3) { create(:food, user: user, name: "テスト食材3", expiration_date: Date.today + 2) }
-  let!(:food_with_image) { create(:food, user: user, name: "画像付きのテスト食材", expiration_date: Date.today + 2, ) }
+  let!(:image) { create(:image) }
   before do
     sign_in(user)
   end
@@ -15,14 +15,10 @@ describe 'FoodのCRUD' do
         expect(page).to have_content food.name
         expect(page).to have_content food.expiration_date
         expect(page).to have_content food.quantity
+        expect(food.food_image.url).to eq('nophoto.png')
         expect(page).to have_content I18n.t("activerecord.attributes.food.storages.#{food.storage}")
       end
-      it '画像が登録されていない場合' do
 
-      end
-      it '画像が登録されてる場合' do
-
-      end
       it '詳細ページへ遷移できる' do
         click_on "詳細", match: :first
         expect(current_path).to eq food_path(food.id)
@@ -59,11 +55,14 @@ describe 'FoodのCRUD' do
 
   #----------------new------------------------
   describe '新規食材の登録' do
+    before do
+      visit new_food_path
+    end
     context 'フォームの入力値が正常な場合' do
       it '登録に成功する' do
         fill_in '食材名', with: 'テスト食材'
         fill_in '在庫数', with: 1
-        fill_in '消費期限', with: Date.today
+        fill_in 'food[expiration_date]', with: Date.today
         click_on '登録する'
         expect(current_path).to eq(foods_path)
         expect(page).to have_text('テスト食材')
@@ -74,7 +73,7 @@ describe 'FoodのCRUD' do
       it '登録に失敗する' do
         fill_in '食材名', with: ''
         fill_in '在庫数', with: 1
-        fill_in '消費期限', with: Date.today
+        fill_in 'food[expiration_date]', with: Date.today
         click_on '登録する'
         expect(current_path).to eq(new_food_path)
         expect(page).to have_text('食材を作成できませんでした')
@@ -84,22 +83,50 @@ describe 'FoodのCRUD' do
     describe '画像の登録' do
       context '新規画像の登録をする場合' do
         it '登録に成功する' do
-
+          fill_in '食材名', with: 'テスト食材'
+          fill_in '在庫数', with: 1
+          fill_in 'food[expiration_date]', with: Date.today
+          attach_file 'food[food_image]', Rails.root.join('spec/fixtures/images/test.jpg')
+          click_on '登録する'
+          expect(page).to have_text('食材を作成しました')
+          food = Food.last
+          expect(food.name).to eq('テスト食材')
+          expect(food.food_image.url).to end_with('test.jpg')
         end
       end
-      context 'デフォルト画像を使用して登録する場合' do
+      context '初期データの画像を使用して登録する場合' do
         it '登録に成功する' do
-
+          fill_in '食材名', with: 'テスト食材'
+          fill_in '在庫数', with: 1
+          fill_in 'food[expiration_date]', with: Date.today
+          select 'テスト初期データ画像', from: 'food[image_id]'
+          click_on '登録する'
+          expect(current_path).to eq(foods_path)
+          expect(page).to have_text('食材を作成しました')
         end
       end
       context '画像を登録しない場合' do
         it 'デフォルト画像が使用される' do
-
+          fill_in '食材名', with: 'テスト食材'
+          fill_in '在庫数', with: 1
+          fill_in 'food[expiration_date]', with: Date.today
+          click_on '登録する'
+          expect(current_path).to eq(foods_path)
+          food = Food.last
+          expect(food.name).to eq('テスト食材')
+          expect(food.food_image.url).to eq('nophoto.png')
         end
       end
       context '新規画像の登録とデフォルト画像を使用して登録が選択されている場合' do
         it '登録に失敗する' do
-
+          fill_in '食材名', with: 'テスト食材'
+          fill_in '在庫数', with: 1
+          fill_in 'food[expiration_date]', with: Date.today
+          attach_file 'food[food_image]', Rails.root.join('spec/fixtures/images/test.jpg')
+          select 'テスト初期データ画像', from: 'food[image_id]'
+          click_on '登録する'
+          expect(current_path).to eq(new_food_path)
+          expect(page).to have_text('画像は1枚だけ選択してください')
         end
       end
     end
