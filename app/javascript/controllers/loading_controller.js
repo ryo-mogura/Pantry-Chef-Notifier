7e3d8de367f487230ecaa44709a6b9a1ac12f093
ryo-mogura <ryosuke48696869@gmail.com>
2024-08-21 19:53:51 +0900

    [feat] #130 レシピ再検索時に前の検索結果を非表示になるようにコードを追加

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -2,13 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading"
 export default class extends Controller {
-  static targets = [ 'loading' ];
-
   showLoading() {
-    this.loadingTarget.classList.remove('hidden');
+    // 検索結果を隠す
+    const results = document.querySelector('[data-loading-target="results"]');
+    if (results) {
+      results.classList.add('hidden'); // 結果を隠す
+    }
+    // ローディングを表示
+    const loading = document.querySelector('[data-loading-target="loading"]');
+    if (loading) {
+      loading.classList.remove('hidden'); // ローディングを表示
+    }
   }
 
-  hideLoading(){
-    this.loadingTarget.classList.add('hidden');
+  hideLoading() {
+    // ローディングを非表示に
+    const loading = document.querySelector('[data-loading-target="loading"]');
+    if (loading) {
+      loading.classList.add('hidden');
+    }
+    // 結果を表示
+    const results = document.querySelector('[data-loading-target="results"]');
+    if (results) {
+      results.classList.remove('hidden'); // 結果を表示
+    }
   }
 }
