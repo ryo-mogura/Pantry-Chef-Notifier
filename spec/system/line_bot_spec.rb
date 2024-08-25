c37b19bbc91ea0921fc5bc8ad3a174e02a5001d0
ryo-mogura <ryosuke48696869@gmail.com>
2024-08-25 22:43:38 +0900

    [test] #129 LINEログインまでをbeforeに追加

--- spec/system/line_bot_spec.rb
+++ spec/system/line_bot_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "LineBot", type: :system do
-  let(:line_user) { create(:user, uid: 1234567890, provider: 'line', email: 'line_user@example.com', ) }
-  describe 'LineBot' do
+  before do
+    Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:line]
+    visit new_user_session_path
+    click_on 'Lineログイン'
+    expect(current_path).to eq authenticated_root_path
+    expect(page).to have_content('ログインしました')
+  end
+
+  fdescribe 'LineBot' do
     describe '食材の登録機能' do
       context '画像付きで登録する場合' do
         it '登録に成功する' do
@@ -23,6 +31,8 @@ RSpec.describe "LineBot", type: :system do
 
       end
       it '食材の削除に失敗する' do
+
+      end
     end
     describe '食材のリスト取得機能' do
       context '食材が登録されている場合' do
@@ -37,7 +47,7 @@ RSpec.describe "LineBot", type: :system do
       end
     end
     describe '消費期限が二日以内の食材の情報を取得' do
-      contxet '消費期限が二日以内の食材が登録されている場合' do
+      context '消費期限が二日以内の食材が登録されている場合' do
         it '消費期限が当日の食材の情報を取得する' do
 
         end
