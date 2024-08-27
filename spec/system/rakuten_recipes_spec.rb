4120f6bbac25b16bceac90cb7bbc6d81a56af7c9
ryo-mogura <ryosuke48696869@gmail.com>
2024-08-27 20:19:07 +0900

    [test] #132 「検索結果が存在しない場合」のテストコードを追加

--- spec/system/rakuten_recipes_spec.rb
+++ spec/system/rakuten_recipes_spec.rb
@@ -6,9 +6,10 @@ describe 'RakutenRecipesController' do
     sign_in(user)
     visit search_rakuten_recipes_path
   end
-
+  
   describe 'レシピ検索' do
     before do
+      # small_categoriesのモックを設定
       allow(RakutenWebService::Recipe).to receive(:small_categories).and_return([
         OpenStruct.new(
           name: 'にんじん',
@@ -37,7 +38,9 @@ describe 'RakutenRecipesController' do
 
     context '検索結果が存在しない場合' do
       it '検索結果がありませんと表示される' do
-        
+        fill_in 'keyword', with: 'カレー'
+        click_on '検索する'
+        expect(page).to have_content('検索結果がありません')
       end
     end
   end
