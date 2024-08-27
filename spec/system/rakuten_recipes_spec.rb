8e3312b1c0241a0bcbd0c312f0cebfc58bee94cf
ryo-mogura <ryosuke48696869@gmail.com>
2024-08-27 20:13:52 +0900

    [test] #132 モックの作成とテストコードを追加

--- spec/system/rakuten_recipes_spec.rb
+++ spec/system/rakuten_recipes_spec.rb
@@ -1,9 +1,31 @@
 require 'rails_helper'
 describe 'RakutenRecipesController' do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in(user)
+    visit search_rakuten_recipes_path
+  end
+
   describe 'レシピ検索' do
+    before do
+      allow(RakutenWebService::Recipe).to receive(:small_categories).and_return([
+        OpenStruct.new(
+          name: 'にんじん',
+          ranking: [
+            OpenStruct.new(title: 'レシピ1', foodImageUrl: 'https://example.com/recipe1.jpg', recipeTitle: 'レシピ1タイトル'),
+            OpenStruct.new(title: 'レシピ2', foodImageUrl: 'https://example.com/recipe2.jpg', recipeTitle: 'レシピ2タイトル')
+          ]
+        )
+      ])
+    end
+
     context '検索ワードが入力されている場合' do
       it '検索結果が表示される' do
-
+        fill_in 'keyword', with: 'にんじん'
+        click_on '検索する'
+        expect(page).to have_content('レシピ1')
+        expect(page).to have_content('レシピ2')
       end
     end
 
@@ -15,7 +37,7 @@ describe 'RakutenRecipesController' do
 
     context '検索結果が存在しない場合' do
       it '検索結果がありませんと表示される' do
-
+        
       end
     end
   end
@@ -35,7 +57,7 @@ describe 'RakutenRecipesController' do
 
       end
       it 'マイページからお気に入りしたレシピが削除されている' do
-        
+
       end
     end
   end
