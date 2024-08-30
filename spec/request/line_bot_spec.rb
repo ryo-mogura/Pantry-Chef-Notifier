c17a5ad660baf0b9743ac55e2d7fd5758489b091
ryo-mogura <ryosuke48696869@gmail.com>
2024-08-30 18:17:55 +0900

    [test] #129 食材リストの取得のテストコードを追加

---
+++ spec/request/line_bot_spec.rb
@@ -0,0 +1,41 @@
+RSpec.describe 'LineBotController', type: :request do
+  describe 'POST /' do
+    let(:user) { create(:user, :line_user) }
+    let(:headers) { { 'X-Line-Signature' => 'test_signature' } }
+    let(:events) do
+      {
+        events: [
+          {
+            type: 'message',
+            replyToken: 'test_token',
+            source: {
+              userId: user.uid
+            },
+            message: {
+              type: 'text',
+              text: '食材リスト'
+            }
+          }
+        ]
+      }
+    end
+
+    let(:client) { instance_double(Line::Bot::Client) }
+
+    before do
+      allow(Line::Bot::Client).to receive(:new).and_return(client)
+      allow(client).to receive(:validate_signature).and_return(true)
+      allow(client).to receive(:reply_message).and_return(true)
+    end
+
+    fit 'responds with the food list message' do
+      post '/', params: events.to_json, headers: headers
+
+      expect(response).to have_http_status(:ok)
+      expect(client).to have_received(:reply_message).with(
+        'test_token',
+        { type: 'text', text: '食材が登録されていません。' }
+      )
+    end
+  end
+end
