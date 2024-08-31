7ee4aa3d6f8baf4c7a00c936bdc970eb5babb65e
ryo-mogura <ryosuke48696869@gmail.com>
2024-08-31 23:07:19 +0900

    [test] #129 データ構造を変更

--- spec/request/line_bot_spec.rb
+++ spec/request/line_bot_spec.rb
@@ -3,21 +3,19 @@ RSpec.describe 'LineBotController', type: :request do
     let(:user) { create(:user, :line_user) }
     let(:headers) { { 'X-Line-Signature' => 'test_signature' } }
     let(:events) do
-      {
-        events: [
-          {
-            type: 'message',
-            replyToken: 'test_token',
-            source: {
-              userId: user.uid
-            },
-            message: {
-              type: 'text',
-              text: '食材リスト'
-            }
+      [
+        {
+          'type' => 'message',
+          'replyToken' => 'test_token',
+          'source' => {
+            'userId' => user.uid
+          },
+          'message' => {
+            'type' => 'text',
+            'text' => '食材リスト'
           }
-        ]
-      }
+        }
+      ]
     end
 
     let(:client) { instance_double(Line::Bot::Client) }
@@ -25,6 +23,7 @@ RSpec.describe 'LineBotController', type: :request do
     before do
       allow(Line::Bot::Client).to receive(:new).and_return(client)
       allow(client).to receive(:validate_signature).and_return(true)
+      allow(client).to receive(:parse_events_from).and_return(events) # ここでparse_events_fromをモック
       allow(client).to receive(:reply_message).and_return(true)
     end
 
