d0bb86bbfbfcca320511d5ef16f1bdaf8bb610b4
ryo-mogura <ryosuke48696869@gmail.com>
2024-09-02 12:31:41 +0900

    [test] #129 現在までのコードとエラー内容を保存

--- spec/request/line_bot_spec.rb
+++ spec/request/line_bot_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'LineBotController', type: :request do
   describe 'POST /' do
     let(:user) { create(:user, :line_user, email: "line_test_#{SecureRandom.uuid}@example.com") }
-    let(:headers) { { 'X-Line-Signature' => 'test_signature' } }
+    let(:headers) { { 'X-Line-Signature' => 'test_signature', 'CONTENT_TYPE' => 'application/json'  } }
     let(:events) do
       [
         {
@@ -18,7 +18,8 @@ RSpec.describe 'LineBotController', type: :request do
       ]
     end
 
-    let(:client) { instance_double(Line::Bot::Client) }
+    #
+    let(:client) { instance_double(Line::Bot::Client, reply_message: true) }
 
     before do
       allow(Line::Bot::Client).to receive(:new).and_return(client)
@@ -28,13 +29,29 @@ RSpec.describe 'LineBotController', type: :request do
     end
 
     fit 'responds with the food list message' do
-      post '/', body: events.to_json, headers: headers
+      post '/', params: events.to_json, headers: headers
 
       expect(response).to have_http_status(:ok)
+
+      puts "Client was called: #{client.inspect}"
       expect(client).to have_received(:reply_message).with(
         'test_token',
         { type: 'text', text: '食材が登録されていません。' }
       )
     end
+    # 9/2 12;30 以下のエラーが発生
+    # 予想問題点
+    # have_received マッチャが正しく機能していない可能性。
+    # テスト内でclientオブジェクトが何らかの理由で期待される呼び出しを認識していない可能性。
+    #   1) LineBotController POST / responds with the food list message
+    #  Failure/Error:
+    #  expect(client).to have_received(:reply_message).with(
+    #    'test_token',
+    #    { type: 'text', text: '食材が登録されていません。' }
+    #  )
+
+    #  (InstanceDouble(Line::Bot::Client) (anonymous)).reply_message("test_token", {:text=>"食材が登録されていません。", :type=>"text"})
+    #      expected: 1 time with arguments: ("test_token", {:text=>"食材が登録されていません。", :type=>"text"})
+    #      received: 0 times
   end
 end
