64016c948b66c339d68d58ee9207323f8387c4a6
ryo-mogura <154300601+ryo-mogura@users.noreply.github.com>
2024-09-07 23:52:16 +0900

    Revert "独自ドメイン #133 close"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << 'www.example.com'
 end
