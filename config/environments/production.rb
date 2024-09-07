bd7f6fe316bdcb91d86dce2a056f7059706f9717
ryo-mogura <ryosuke48696869@gmail.com>
2024-09-08 02:34:23 +0900

    [bug] #159 カスタムドメインの設定でwwwが抜けていたので修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << 'pantry-chef-notifier.com'
+  config.hosts << 'www.pantry-chef-notifier.com'
 end
