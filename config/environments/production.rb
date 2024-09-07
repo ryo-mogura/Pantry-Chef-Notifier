b25c7a6a249e1d60dc98ea13e5e825d66bf68a02
ryo-mogura <ryosuke48696869@gmail.com>
2024-09-07 23:52:59 +0900

    [feat] #133 間違っていたため修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << 'www.example.com'
+  config.hosts << 'pantry-chef-notifier.com'
 end
