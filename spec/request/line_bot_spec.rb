5fc1cc1647c27c1ec7b25b00413795bb759ad9c1
ryo-mogura <ryosuke48696869@gmail.com>
2024-09-23 23:20:22 +0900

    [test] #168 LineAPIのモック・スタブ化 1. LINE Messaging APIの認証情報をテスト用に 2. Line::Bot::Clientクラスのインスタンスをモック化 3. クライアントインスタンスのメソッドをスタブ化

--- spec/request/line_bot_spec.rb
+++ spec/request/line_bot_spec.rb
@@ -7,5 +7,21 @@ RSpec.describe 'LineBotController', type: :request do
     let(:signature) { 'test_signature' }
     let(:line_channel_secret) { 'test_channel_secret' }
     let(:line_channel_token) { 'test_channel_token' }
+
+    before do
+      # LINE Messaging APIの認証情報をテスト用に差し替え
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with('LINE_CHANNEL_SECRET').and_return(line_channel_secret)
+      allow(ENV).to receive(:[]).with('LINE_CHANNEL_TOKEN').and_return(line_channel_token)
+
+      # Line::Bot::Clientのインスタンスをモック化
+      @client_instance = instance_double('Line::Bot::Client')
+      allow(Line::Bot::Client).to receive(:new).and_return(@client_instance)
+
+      # クライアントインスタンスのメソッドをスタブ化
+      allow(@client_instance).to receive(:validate_signature).and_return(true)
+      allow(@client_instance).to receive(:parse_events_from).and_return([line_event])
+      allow(@client_instance).to receive(:reply_message).and_return(nil)
+    end
   end
 end
