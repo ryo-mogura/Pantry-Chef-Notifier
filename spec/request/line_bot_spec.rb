caa0635f6e0de75b3655222091ea6f809a600295
ryo-mogura <ryosuke48696869@gmail.com>
2024-09-23 22:37:56 +0900

    [test] #172 食材リストの取得

--- spec/request/line_bot_spec.rb
+++ spec/request/line_bot_spec.rb
@@ -8,5 +8,37 @@ RSpec.describe 'LineBotController', type: :request do
     let(:line_channel_secret) { 'test_channel_secret' }
     let(:line_channel_token) { 'test_channel_token' }
 
+    before do
+      # LINE Messaging APIの認証情報をテスト用に差し替え
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with('LINE_CHANNEL_SECRET').and_return(line_channel_secret)
+      allow(ENV).to receive(:[]).with('LINE_CHANNEL_TOKEN').and_return(line_channel_token)
+
+      # Line::Bot::Clientのインスタンスをスタブ化
+      @client_instance = instance_double('Line::Bot::Client')
+      allow(Line::Bot::Client).to receive(:new).and_return(@client_instance)
+
+      # クライアントインスタンスのメソッドをスタブ
+      allow(@client_instance).to receive(:validate_signature).and_return(true)
+      allow(@client_instance).to receive(:parse_events_from).and_return([line_event])
+      allow(@client_instance).to receive(:reply_message).and_return(nil)
+    end
+
+    context 'ユーザーが食材リストを送信したとき' do
+      let(:line_event) do
+        Line::Bot::Event::Message.new(
+          'replyToken' => 'dummy_reply_token',
+          'type' => 'message',
+          'timestamp' => Time.now.to_i * 1000,
+          'source' => { 'type' => 'user', 'userId' => line_user_id },
+          'message' => { 'id' => '325708', 'type' => 'text', 'text' => message_text }
+        )
+      end
+      it '食材リストを返信する' do
+        post '/', params: '', headers: { 'HTTP_X_LINE_SIGNATURE' => signature }
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
   end
 end
