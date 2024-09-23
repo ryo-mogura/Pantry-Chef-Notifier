66846db0dd0feb0d64fd6732d2f51d477998f5e4
ryo-mogura <ryosuke48696869@gmail.com>
2024-09-23 21:37:29 +0900

    [feat] #172 テストデータの作成

--- spec/request/line_bot_spec.rb
+++ spec/request/line_bot_spec.rb
@@ -1,32 +1,12 @@
 RSpec.describe 'LineBotController', type: :request do
-  fdescribe 'LineBot API' do
-    let(:valid_signature) { 'valid-signature' }
-    let(:user) { create(:user, uid: 'U1234567890', provider: 'line') }
-    let(:food_item) { create(:food, user: user, name: 'りんご', expiration_date: Date.today) }
+  fdescribe 'POST #callback' do
+    let(:line_user_id) { 'U1234567890abcdef1234567890abcdef' }
+    let(:user) { create(:user, uid: line_user_id) }
+    let!(:food) { create(:food, user: user, name: 'りんご', expiration_date: Date.today) }
+    let(:message_text) { '食材リスト' }
+    let(:signature) { 'test_signature' }
+    let(:line_channel_secret) { 'test_channel_secret' }
+    let(:line_channel_token) { 'test_channel_token' }
 
-    before do
-      allow_any_instance_of(Line::Bot::Client).to receive(:validate_signature).and_return(true)
-      allow_any_instance_of(Line::Bot::Client).to receive(:reply_message).and_return(true)
-    end
-
-    describe 'POST /callback' do
-      it '「食材リスト」を送信した際に正しいリストが返される' do
-        post '/',
-          params: line_request_with_text('食材リスト').to_json,
-          headers: { 'X-Line-Signature' => valid_signature, 'Content-Type' => 'application/json' }
-
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to include('食材名: りんご 在庫数: 3')
-      end
-
-      it '「食材リスト」を送信した際、食材が登録されていない場合に適切なメッセージが返されるか' do
-        post '/',
-          params: line_request_with_text('食材リスト').to_json,
-          headers: { 'X-Line-Signature' => valid_signature, 'Content-Type' => 'application/json' }
-
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to include('食材が登録されていません。')
-      end
-    end
   end
 end
